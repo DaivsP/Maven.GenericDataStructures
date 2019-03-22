7cff07eed7c91555635881aa2ed8daaab0c71a98
Davis <davisperchik@gmail.com>
2019-03-21 21:32:46 -0400

    all tests passing, done

--- src/main/java/TableNested/TableNested.java
+++ src/main/java/TableNested/TableNested.java
@@ -1,5 +1,7 @@
 package TableNested;
 
+import Table.Entry;
+
 import java.util.ArrayList;
 
 /**
@@ -7,5 +9,42 @@ import java.util.ArrayList;
  * Think about how nested classes should work with generics.
  */
 public class TableNested<K, V> {
+    private ArrayList<Entry> entries;
 
+    public TableNested(){
+        entries = new ArrayList<>();
+    }
+
+    public V get(K key) {
+        for (Entry entry : entries) {
+            if (entry.key.equals(key)){
+                return entry.value;
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value) {
+        remove(key);
+        entries.add(new Entry(key, value));
+    }
+
+    public void remove(K key) {
+        for (Entry entry : entries) {
+            if (entry.key.equals(key)){
+                entries.remove(entry);
+                break;
+            }
+        }
+    }
+
+    private class Entry{
+        private K key;
+        private V value;
+
+        public Entry(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
